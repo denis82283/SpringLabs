928048c515dffd41b2d64d3f238d3f058954eff4
denis82283 <denis.romanyuk0629@gmail.com>
2021-07-21 16:55:11 +0300

    creating project

---
+++ src/test/java/en/app/springlabs/SpringLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package en.app.springlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
